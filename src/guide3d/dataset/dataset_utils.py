edc39c79acbbc2253228b2d6b9969de886ed4bf4
Tudor Jianu <tudorjnu@gmail.com>
2024-08-21 11:46:22 +0100

    add doctring

--- src/guide3d/dataset/dataset_utils.py
+++ src/guide3d/dataset/dataset_utils.py
@@ -10,6 +10,40 @@ from torch.utils import data
 
 
 class Guide3D(data.Dataset):
+    """
+    A custom PyTorch Dataset class for loading and processing Guide3D annotation data.
+
+    This dataset class supports downloading the dataset if it does not exist locally,
+    processing the data, and splitting it into train, validation, and test sets.
+
+    Attributes:
+        dataset_path (Path): Path to the dataset directory.
+        annotations_file (Path): Path to the annotations file.
+        raw_data (Dict): Raw data loaded from the annotations file.
+        data (List): Processed data corresponding to the selected split.
+
+    Args:
+        dataset_path (Union[Path, str]): Path to the dataset directory. If the path is a string,
+                                         it will be converted to a `Path` object.
+        annotations_file (Union[Path, str]): Path to the JSON file containing annotations.
+                                             If the path is a string, it will be resolved
+                                             relative to the annotations directory.
+        process_data (callable): Function to process the raw annotation data.
+        split_fn (callable): Function to split the processed data into train, val, and test sets.
+        download (bool, optional): If True, download the dataset if it does not exist locally.
+                                   Defaults to False.
+        split (str, optional): Which data split to load. Must be one of "train", "val", or "test".
+                               Defaults to "train".
+        split_ratio (tuple, optional): A tuple indicating the ratio of train, val, and test splits.
+                                       Must have three elements. Defaults to (0.8, 0.1, 0.1).
+
+    Raises:
+        AssertionError: If the split is not one of "train", "val", or "test".
+        AssertionError: If the length of split_ratio is not 3.
+        AssertionError: If the dataset path does not exist and download is False.
+        AssertionError: If the annotations file does not exist.
+    """
+
     def __init__(
         self,
         dataset_path: Union[Path, str],
@@ -43,7 +77,7 @@ class Guide3D(data.Dataset):
             self.annotations_file = self.annotations_dir / annotations_file
             if not self.annotations_file.exists():
                 raise AssertionError(
-                    f"Annotation file should be one of: {[f for f in self.annotations_dir.iterdir()]}, got {self.annotations_file}"
+                    f"Annotation file should be one of: {[f.name for f in self.annotations_dir.iterdir()]}, got {self.annotations_file.name}"
                 )
         else:
             self.annotations_file = annotations_file
